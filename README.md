af755c150c6f136f18eb9cf8740c35b2e65708bf
jokerzserenity <jelle.swaelen@immalle.be>
2017-01-11 15:26:07 +0100

    Closes #1, minor design changes, everything's responsive

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 All files related to and used on the website can be found here.
 
 ## 2017-01-06
-+ Repository created, added basic information.
-+ Basic html with header added.
++ Repository created, added basic information
++ Basic html with header added
 
 ## 2017-01-10
 + Content and basic lay-out added
+
+## 2017-01-11
++ Minor changes to the overall design
++ Menu works, fixed #1
++ Added button and text box to sign up with e-mail (doesn't work yet)
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $("#homeButton").click(function() {
     $('html,body').animate({
         scrollTop: $(".home").offset().top},
-        'slow');
+        1500);
 });
 
 $("#eventsButton").click(function() {
     $('html,body').animate({
         scrollTop: $(".events").offset().top},
-        'slow');
+        1500);
 });
 
 $("#joinButton").click(function() {
     $('html,body').animate({
         scrollTop: $(".join").offset().top},
-        'slow');
+        1500);
 });
 
 $("#contactButton").click(function() {
     $('html,body').animate({
         scrollTop: $(".contact").offset().top},
-        'slow');
+        1500);
 });
